4eadfc78ccbe794a86501aea035f0f40401cc1e5
Channon Zuo <channon.z2020@gmail.com>
2023-02-10 12:07:40 -0800

    Create CipherEncryptDecrypt.py
    Initial commit with "Hello, World!" print statement

---
+++ CipherEncryptDecrypt.py
@@ -0,0 +1,8 @@
+__author__      = "Channon Zuo"
+__email__       = "01channonzuo@gmail.com"
+__published__   = "02/10/19"
+__updated__     = "02/10/19"
+__version__     = "1.0.0"
+
+if __name__ == "__main__":
+    print("Hello, World!")
